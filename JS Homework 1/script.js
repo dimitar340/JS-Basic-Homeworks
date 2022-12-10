f085b3f952fa1579cfb091ac970230dcd6d60f02
Dimitar Bogdanov <Dimitar340@gmail.com>
2022-12-10 14:52:24 +0100

    Add files via upload

---
+++ JS Homework 1/script.js
@@ -0,0 +1,9 @@
+console.log('----- JS Homework 1 -----');
+
+var Iphone = 119.95;
+var IphoneCount = 30;
+var Tax = 5/100;
+var total_price = Iphone * IphoneCount + (Iphone*IphoneCount*Tax);
+
+
+console.log (`If one Iphone cost 119.95, the total amount Iphones is 30, and Tax is 5%, then the total price is ${total_price}.`);
